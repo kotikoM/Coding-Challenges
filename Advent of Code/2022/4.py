b6fc323f2181363f0c8784fe7676eaa1e444c835
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-09 20:51:09 +0400

    Solve AoC 2022 - Day 4 Part 1

---
+++ Advent of Code/2022/4.py
@@ -0,0 +1,12 @@
+def contains(a1, a2, b1, b2):
+    return ((b1 <= a1 <= b2 and b1 <= a2 <= b2) or
+            (a1 <= b1 <= a2 and a1 <= b2 <= a2))
+
+
+total = 0
+for line in open('input').read().splitlines():
+    a1, a2, b1, b2 = map(int, line.replace(',', '-').split('-'))
+    if contains(int(a1), int(a2), int(b1), int(b2)):
+        total += 1
+
+print(total)
