20b0890f2c7541867ca69c9794713550b946b80a
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-09 15:24:47 +0400

    finished AoC:23 24:p2

--- Advent of Code/2023/24.py
+++ Advent of Code/2023/24.py
@@ -1,3 +1,6 @@
+from sympy import symbols, solve
+
+
 class HailStone:
     def __init__(self, x, y, z, dx, dy, dz):
         self.x = x
@@ -7,48 +10,20 @@ class HailStone:
         self.dy = dy
         self.dz = dz
 
-        # y = kx + b
-        self.k = dy / dx
-        self.b = y - self.k * x
-
-    def __repr__(self):
-        return f'({self.k}, {self.b})'
-
-
-def find_intersection_point(a, b):
-    x = (b.b - a.b) / (a.k - b.k)
-    y = a.k * x + a.b
-    return x, y
-
-
-def in_past(a, x, y):
-    return ((a.x - x) / a.dx) > 0 and ((a.y - y) / a.dy) > 0
-
 
 hailStones = [HailStone(*map(int, line.replace(' @', ',').split(', '))) for line in open('input')]
-print(hailStones)
+# looking for sx, sy, sz, sdx, sdy, sdz such that at some points t we intersect all hailstones
+# 6 unknowns
+# governing equations that must be met
+# t = (x - sx) / (sdx - dx)
+# t = (y - sy) / (sdy - dy)
+# t = (z - sz) / (sdz - dz)
 
-lower_limit = 200000000000000
-higher_limit = 400000000000000
-intersections = []
-for i in range(len(hailStones) - 1):
-    for j in range(i + 1, len(hailStones)):
-        a = hailStones[i]
-        b = hailStones[j]
-        if a.k == b.k: continue
+equations = []
+sx, sy, sz, sdx, sdy, sdz, t = symbols('sx sy sz sdx sdy sdz t')
+for h in hailStones[3:]:
+    equations.append((sx - h.x) * (h.dy - sdy) - (sy - h.y) * (h.dx - sdx))
+    equations.append((sy - h.y) * (h.dz - sdz) - (sz - h.z) * (h.dy - sdy))
 
-        x, y = find_intersection_point(a, b)
-        print(x, y)
-        if not (lower_limit <= x <= higher_limit and lower_limit <= y <= higher_limit):
-            print('not inside limits')
-            continue
-        print('inside limits')
-
-        if in_past(a, x, y) or in_past(b, x, y):
-            print('in the past')
-            continue
-
-        intersections.append((x, y))
-
-print(intersections)
-print(len(intersections))
+solution = solve(equations)
+print(sum(list(solution[0].values())[3:]))
