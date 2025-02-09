5be801d667ea57e5abee047ca6bc3db17a620a3a
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-09 20:51:47 +0400

    Solve AoC 2022 - Day 4 Part 2

--- Advent of Code/2022/4.py
+++ Advent of Code/2022/4.py
@@ -1,6 +1,6 @@
 def contains(a1, a2, b1, b2):
-    return ((b1 <= a1 <= b2 and b1 <= a2 <= b2) or
-            (a1 <= b1 <= a2 and a1 <= b2 <= a2))
+    return (b1 <= a1 <= b2 or b1 <= a2 <= b2 or
+            a1 <= b1 <= a2 or a1 <= b2 <= a2)
 
 
 total = 0
