7b3c7148d97ebd89b0396509bb1fc036edb50631
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-10 12:11:45 +0400

    Solve AoC 2022 - Day 7 Part 1

---
+++ Advent of Code/2022/7.py
@@ -0,0 +1,84 @@
+class Directory:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.size = 0
+        self.children = []
+        self.files = []
+
+    def add_child(self, child):
+        self.children.append(child)
+        self.update_size()
+
+    def add_file(self, file):
+        self.files.append(file)
+        self.update_size()
+
+    def update_size(self):
+        self.size = sum(file[1] for file in self.files) + sum(child.size for child in self.children)
+        if self.parent:
+            self.parent.update_size()
+
+    def get_parent(self):
+        return self.parent
+
+    def get_child(self, name):
+        for child in self.children:
+            if child.name == name:
+                return child
+        return None
+
+    def __repr__(self, level=0):
+        indent = "    " * level
+        result = f"{indent}📂 {self.name} {self.size}\n"
+
+        for file in self.files:
+            result += f"{indent}    📄 {file}\n"
+
+        for child in self.children:
+            result += child.__repr__(level + 1)
+
+        return result
+
+
+root = Directory('/', None)
+current_dir = root
+lines = open('input').read().splitlines()
+
+pointer = 0
+while pointer < len(lines):
+    line = lines[pointer]
+    if line.startswith('$ cd'):
+        if line == '$ cd /':
+            current_dir = root
+        elif line == '$ cd ..':
+            current_dir = current_dir.get_parent()
+        else:
+            current_dir = current_dir.get_child(line[5:])
+            assert current_dir is not None
+        pointer += 1
+
+    elif line.startswith('$ ls'):
+        pointer += 1
+        while pointer < len(lines) and not lines[pointer].startswith('$'):
+            line = lines[pointer]
+            if line.startswith('dir'):
+                current_dir.add_child(Directory(line[4:], current_dir))
+            else:
+                size, name = line.split()
+                current_dir.add_file((name, int(size)))
+
+            pointer += 1
+
+
+num = 0
+def find_dirs_more_than_100k(dir):
+    global num
+    if dir.size <= 100000:
+        num += dir.size
+
+    for child in dir.children:
+        find_dirs_more_than_100k(child)
+
+find_dirs_more_than_100k(root)
+print(num)
