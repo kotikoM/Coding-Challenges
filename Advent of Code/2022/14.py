8059ed647568b83c07243794940d8946badc0e65
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-11 17:09:20 +0400

    Solve AoC 2022 - Day 14 Part 1

---
+++ Advent of Code/2022/14.py
@@ -0,0 +1,37 @@
+rocks = set()
+
+abyss = 0
+
+for line in open('input'):
+    points = [tuple(map(int, p.split(","))) for p in line.strip().split(" -> ")]
+    for (x1, y1), (x2, y2) in zip(points, points[1:]):
+        x1, x2 = sorted([x1, x2])
+        y1, y2 = sorted([y1, y2])
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                rocks.add((x, y))
+                abyss = max(abyss, y + 1)
+
+n = 0
+while True:
+    s = (500, 0)
+
+    while True:
+        x, y = s
+        if y >= abyss:
+            print(n)
+            exit(0)
+
+        if (x, y + 1) not in rocks:
+            s = (x, y + 1)
+            continue
+        if (x - 1, y + 1) not in rocks:
+            s = (x - 1, y + 1)
+            continue
+        if (x + 1, y + 1) not in rocks:
+            s = (x + 1, y + 1)
+            continue
+
+        rocks.add(s)
+        n += 1
+        break
