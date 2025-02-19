1651bac35f02257fe12b8b8b09fa4ef8a75895d0
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-19 17:40:49 +0400

    Solve AoC 2022 - Day 21 Part 2

--- Advent of Code/2022/21.py
+++ Advent of Code/2022/21.py
@@ -1,20 +1,27 @@
-monkeys = {}
-for line in open('input').read().splitlines():
-    name, action = line.split(': ')
-    if any(op in action for op in '+-*/'):
-        monkeys[name] = ('op', action)
+import sympy
+
+monkeys = {"humn": sympy.Symbol("x")}
+
+ms = [line.strip() for line in open('input')]
+
+ops = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y,
+}
+
+for m in ms:
+    name, expr = m.split(": ")
+    if name in monkeys: continue
+    if expr.isdigit():
+        monkeys[name] = sympy.Integer(expr)
     else:
-        monkeys[name] = ('val', int(action))
-
-
-def evaluate(name):
-    type, action = monkeys[name]
-
-    if type == 'val':
-        return action
-    else:
-        m1, op, m2 = action.split()
-        return eval(str(evaluate(m1)) + op + str(evaluate(m2)))
-
-
-print(evaluate('root'))
+        left, op, right = expr.split()
+        if left in monkeys and right in monkeys:
+            if name == "root":
+                print(sympy.solve(monkeys[left] - monkeys[right])[0])
+                break
+            monkeys[name] = ops[op](monkeys[left], monkeys[right])
+        else:
+            ms.append(m)
