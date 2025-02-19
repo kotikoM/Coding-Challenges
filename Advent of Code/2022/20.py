e808afe804fc1506476fd3a0da727b3c20ced738
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-19 16:09:42 +0400

    Solve AoC 2022 - Day 20 Part 2

--- Advent of Code/2022/20.py
+++ Advent of Code/2022/20.py
@@ -5,7 +5,7 @@ class Node:
         self.next = None
 
 
-nums = [Node(int(x)) for x in open('input')]
+nums = [Node(int(x) * 811589153) for x in open('input')]
 
 for i in range(len(nums)):
     nums[i].next = nums[(i + 1) % len(nums)]
@@ -13,35 +13,36 @@ for i in range(len(nums)):
 
 mod = len(nums) - 1
 
-for current in nums:
-    if current.num == 0:
-        target = current
-        continue
-
-    temp = current
-    if current.num > 0:
-        for _ in range(current.num % mod):
-            temp = temp.next
-        if current == temp:
+for _ in range(10):
+    for current in nums:
+        if current.num == 0:
+            target = current
             continue
-        current.next.prev = current.prev
-        current.prev.next = current.next
-        temp.next.prev = current
-        current.next = temp.next
-        temp.next = current
-        current.prev = temp
 
-    else:
-        for _ in range(-current.num % mod):
-            temp = temp.prev
-        if current == temp:
-            continue
-        current.prev.next = current.next
-        current.next.prev = current.prev
-        temp.prev.next = current
-        current.prev = temp.prev
-        temp.prev = current
-        current.next = temp
+        temp = current
+        if current.num > 0:
+            for _ in range(current.num % mod):
+                temp = temp.next
+            if current == temp:
+                continue
+            current.next.prev = current.prev
+            current.prev.next = current.next
+            temp.next.prev = current
+            current.next = temp.next
+            temp.next = current
+            current.prev = temp
+
+        else:
+            for _ in range(-current.num % mod):
+                temp = temp.prev
+            if current == temp:
+                continue
+            current.prev.next = current.next
+            current.next.prev = current.prev
+            temp.prev.next = current
+            current.prev = temp.prev
+            temp.prev = current
+            current.next = temp
 
 total = 0
 for _ in range(3):
