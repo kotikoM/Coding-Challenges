69a937990ee0098a9009f9151df785850f531979
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-23 22:12:50 +0400

    Solve AoC 2022 - Day 22 Part 2

--- Advent of Code/2022/22.py
+++ Advent of Code/2022/22.py
@@ -25,15 +25,53 @@ while grid[x][y] != ".":
 for steps, dir in re.findall(r"(\d+)([RL]?)", sequence):
     steps = int(steps)
     for _ in range(steps):
-        nx = x
-        ny = y
-        while True:
-            nx = (nx + dx) % len(grid)
-            ny = (ny + dy) % len(grid[0])
-            if grid[nx][ny] != " ":
-                break
-
+        cdx = dx
+        cdy = dy
+        nx = x + dx
+        ny = y + dy
+        if nx < 0 and 50 <= ny < 100 and dx == -1:
+            dx, dy = 0, 1
+            nx, ny = ny + 100, 0
+        elif ny < 0 and 150 <= nx < 200 and dy == -1:
+            dx, dy = 1, 0
+            nx, ny = 0, nx - 100
+        elif nx < 0 and 100 <= ny < 150 and dx == -1:
+            nx, ny = 199, ny - 100
+        elif nx >= 200 and 0 <= ny < 50 and dx == 1:
+            nx, ny = 0, ny + 100
+        elif ny >= 150 and 0 <= nx < 50 and dy == 1:
+            dy = -1
+            nx, ny = 149 - nx, 99
+        elif ny == 100 and 100 <= nx < 150 and dy == 1:
+            dy = -1
+            nx, ny = 149 - nx, 149
+        elif nx == 50 and 100 <= ny < 150 and dx == 1:
+            dx, dy = 0, -1
+            nx, ny = ny - 50, 99
+        elif ny == 100 and 50 <= nx < 100 and dy == 1:
+            dx, dy = -1, 0
+            nx, ny = 49, nx + 50
+        elif nx == 150 and 50 <= ny < 100 and dx == 1:
+            dx, dy = 0, -1
+            nx, ny = ny + 100, 49
+        elif ny == 50 and 150 <= nx < 200 and dy == 1:
+            dx, dy = -1, 0
+            nx, ny = 149, nx - 100
+        elif nx == 99 and 0 <= ny < 50 and dx == -1:
+            dx, dy = 0, 1
+            nx, ny = ny + 50, 50
+        elif ny == 49 and 50 <= nx < 100 and dy == -1:
+            dx, dy = 1, 0
+            nx, ny = 100, nx - 50
+        elif ny == 49 and 0 <= nx < 50 and dy == -1:
+            dy = 1
+            nx, ny = 149 - nx, 0
+        elif ny < 0 and 100 <= nx < 150 and dy == -1:
+            dy = 1
+            nx, ny = 149 - nx, 50
         if grid[nx][ny] == "#":
+            dx = cdx
+            dy = cdy
             break
 
         x = nx
