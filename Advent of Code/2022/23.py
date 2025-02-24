e48afffb9e756ed712a9900ca9a2aaad79a2098f
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-24 11:12:00 +0400

    Solve AoC 2022 - Day 23 Part 2

--- Advent of Code/2022/23.py
+++ Advent of Code/2022/23.py
@@ -6,7 +6,9 @@ for i, l in enumerate(open('input').read().splitlines()):
 
 directions = ['N', 'S', 'W', 'E']
 
-for _ in range(20):
+round = 0
+while True:
+    round += 1
     proposed_positions = {}
     for idx, (x, y) in enumerate(elves):
         N, S, W, E = (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)
@@ -16,20 +18,25 @@ for _ in range(20):
             proposed_positions[idx] = (x, y)
             continue
 
+        moved = False
         for d in directions:
             if d == 'N' and all(p not in elves for p in [N, NE, NW]):
                 proposed_positions[idx] = N
+                moved = True
                 break
             elif d == 'S' and all(p not in elves for p in [S, SE, SW]):
                 proposed_positions[idx] = S
+                moved = True
                 break
             elif d == 'W' and all(p not in elves for p in [W, NW, SW]):
                 proposed_positions[idx] = W
+                moved = True
                 break
             elif d == 'E' and all(p not in elves for p in [E, NE, SE]):
                 proposed_positions[idx] = E
+                moved = True
                 break
-        else:
+        if not moved:
             proposed_positions[idx] = (x, y)
 
     new_positions = {}
@@ -39,10 +46,10 @@ for _ in range(20):
         else:
             new_positions[idx] = elves[idx]
 
+    if new_positions == {idx: pos for idx, pos in enumerate(elves)}:
+        break
+
     elves = [new_positions[idx] for idx in range(len(elves))]
     directions.append(directions.pop(0))
 
-xs, ys = zip(*elves)
-width = max(ys) - min(ys) + 1
-height = max(xs) - min(xs) + 1
-print(width * height - len(elves))
+print(round)
