68d8bdaa33de4a40f02546b8152ae8644ce8e414
Konstantine Machavariani <kotiko2004@gmail.com>
2025-02-25 10:33:46 +0400

    Solve AoC 2022 - Day 24 Part 2

--- Advent of Code/2022/24.py
+++ Advent of Code/2022/24.py
@@ -8,14 +8,14 @@ for x, line in enumerate(open('input').read().splitlines()[1:]):
         if item in "<>^v":
             blizzards["<>^v".find(item)].add((x, y))
 
-queue = deque([(0, -1, 0)])
+queue = deque([(0, -1, 0, 0)])
 seen = set()
-target = (x, y - 1)
+target = [(x, y - 1), (-1, 0)]
 
 lcm = x * y // math.gcd(x, y)
 
 while queue:
-    time, cx, cy = queue.popleft()
+    time, cx, cy, stage = queue.popleft()
 
     time += 1
 
@@ -23,26 +23,29 @@ while queue:
         nx = cx + dx
         ny = cy + dy
 
-        if (nx, ny) == target:
-            print(time)
-            exit(0)
+        nstage = stage
+        if (nx, ny) == target[stage % 2]:
+            if stage == 2:
+                print(time)
+                exit(0)
+            nstage += 1
 
-        if (nx < 0 or ny < 0 or nx >= x or ny >= y) and not (nx, ny) == (-1, 0):
+        if (nx < 0 or ny < 0 or nx >= x or ny >= y) and (nx, ny) not in target:
             continue
 
         fail = False
 
-        if (nx, ny) != (-1, 0):
+        if (nx, ny) not in target:
             for i, tx, ty in ((0, 0, -1), (1, 0, 1), (2, -1, 0), (3, 1, 0)):
                 if ((nx - tx * time) % x, (ny - ty * time) % y) in blizzards[i]:
                     fail = True
                     break
 
         if not fail:
-            key = (nx, ny, time % lcm)
+            key = (nx, ny, nstage, time % lcm)
 
             if key in seen:
                 continue
 
             seen.add(key)
-            queue.append((time, nx, ny))
+            queue.append((time, nx, ny, nstage))
