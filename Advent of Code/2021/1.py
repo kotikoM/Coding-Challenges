6b7ae3252dc711e84eb43685a48c176a5d7a3607
Konstantine Machavariani <kotiko2004@gmail.com>
2025-04-09 17:26:34 +0400

    Solve AoC 2021 - Day 1 Part 2

--- Advent of Code/2021/1.py
+++ Advent of Code/2021/1.py
@@ -1,2 +1,5 @@
 nums = list(map(int, open('input')))
-print(sum(b > a for a, b in zip(nums, nums[1:])))
+print(sum(b > a for a, b in zip(
+    map(sum, zip(nums, nums[1:], nums[2:])),
+    map(sum, zip(nums[1:], nums[2:], nums[3:]))
+)))
