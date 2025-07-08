620f843be900aaef75f1ec28284ac53eb63d86af
Konstantine Machavariani <kotiko2004@gmail.com>
2025-07-08 13:50:41 +0400

    Solve AoC 2021 - Day 7 Part 2

--- Advent of Code/2021/7.py
+++ Advent of Code/2021/7.py
@@ -1,12 +1,16 @@
 positions = list(map(int, open('input').read().split(',')))
 positions.sort()
 
-n = len(positions)
-if n % 2 == 1:
-    median = positions[n // 2]
-else:
-    median = (positions[n // 2 - 1] + positions[n // 2]) // 2
+def costs(pos, target):
+    n = abs(pos - target)
+    return n * (n + 1) // 2
 
-fuel = sum(abs(p - median) for p in positions)
+min_fuel = float('inf')
+min_value = positions[0]
+for i in range(len(positions)):
+    fuel = sum(costs(pos, positions[i]) for pos in positions)
+    if fuel < min_fuel:
+        min_fuel = fuel
+        min_value = positions[i]
 
-print(fuel)
+print(min_fuel)
