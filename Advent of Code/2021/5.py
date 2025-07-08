d0d0f963733f50c4b4dd22a188ef89a645b163d3
Konstantine Machavariani <kotiko2004@gmail.com>
2025-07-08 12:54:21 +0400

    Solve AoC 2021 - Day 5 Part 1

---
+++ Advent of Code/2021/5.py
@@ -0,0 +1,12 @@
+grid = {}
+
+for line in open('input').read().splitlines():
+    l, r = line.split(" -> ")
+    x1, y1 = map(int, l.split(","))
+    x2, y2 = map(int, r.split(","))
+    if x1 == x2 or y1 == y2:
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                grid[(x, y)] = grid.get((x, y), 0) + 1
+
+print(len([v for v in grid.values() if v > 1]))
