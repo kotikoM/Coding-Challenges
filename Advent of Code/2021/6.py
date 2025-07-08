8b32f30a5a5f0ff8c4be1464dfa356f3e55cbdb3
Konstantine Machavariani <kotiko2004@gmail.com>
2025-07-08 13:18:38 +0400

    Solve AoC 2021 - Day 6 Part 1

---
+++ Advent of Code/2021/6.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+initial_fish = list(map(int, open('input').read().strip().split(",")))
+
+
+@lru_cache(maxsize=None)
+def simulate(timer, days_left):
+    if days_left < timer:
+        return 1
+    else:
+        # One fish spawns another at (days_left - timer - 1)
+        # Add this fish + all its descendants
+        return simulate(6, days_left - timer - 1) + simulate(8, days_left - timer - 1)
+
+
+days = 80
+total = sum(simulate(timer, days - 1) for timer in initial_fish)
+print(total)
