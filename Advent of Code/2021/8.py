bae1cfbf79ceaf7d3f702c1cbb21831a291c51b2
Konstantine Machavariani <kotiko2004@gmail.com>
2025-07-28 21:03:02 +0400

    Solve AoC 2021 - Day 8 Part 2

--- Advent of Code/2021/8.py
+++ Advent of Code/2021/8.py
@@ -1,15 +1,44 @@
-#  aaaa
-# b    c
-# b    c
-#  dddd
-# e    f
-# e    f
-#  gggg
+def decode_line(ten_signals, output_values):
+    patterns = [''.join(sorted(p)) for p in ten_signals]
+    patterns_set = [set(p) for p in patterns]
+
+    digits = {}
+
+    for p in patterns_set:
+        if len(p) == 2:
+            digits[1] = p
+        elif len(p) == 3:
+            digits[7] = p
+        elif len(p) == 4:
+            digits[4] = p
+        elif len(p) == 7:
+            digits[8] = p
+
+    for p in patterns_set:
+        if len(p) == 5:  # 2, 3, 5
+            if digits[1] <= p:
+                digits[3] = p
+            elif len(p & digits[4]) == 3:
+                digits[5] = p
+            else:
+                digits[2] = p
+        elif len(p) == 6:  # 0, 6, 9
+            if digits[4] <= p:
+                digits[9] = p
+            elif digits[1] <= p:
+                digits[0] = p
+            else:
+                digits[6] = p
+
+    pattern_to_digit = {''.join(sorted(v)): str(k) for k, v in digits.items()}
+
+    output_number = int(''.join([pattern_to_digit[''.join(sorted(o))] for o in output_values]))
+    return output_number
+
 
 total = 0
 for l in open('input').read().split('\n'):
-    ten, four = l.split(' | ')
-    ten, four = ten.split(), four.split()
-    total += sum([1 if len(s) in (2, 3, 4, 7) else 0 for s in four])
+    ten, four = l.strip().split(' | ')
+    total += decode_line(ten.split(), four.split())
 
 print(total)
