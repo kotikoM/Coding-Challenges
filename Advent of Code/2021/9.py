74bb56a311516fe80040d1b2559c6c864eceaf15
Konstantine Machavariani <kotiko2004@gmail.com>
2025-07-28 21:43:54 +0400

    Solve AoC 2021 - Day 9 Part 2

--- Advent of Code/2021/9.py
+++ Advent of Code/2021/9.py
@@ -1,14 +1,37 @@
+from collections import deque
+from math import prod
+
 grid = [list(map(int, l.strip())) for l in open('input').read().split()]
+dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 low_points = []
 for x in range(len(grid)):
     for y in range(len(grid[x])):
         neighbors = []
-        for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+        for dx, dy in dirs:
             if 0 <= x + dx < len(grid) and 0 <= y + dy < len(grid[x]):
                 neighbors.append(grid[x + dx][y + dy])
 
         if all(grid[x][y] < n for n in neighbors):
             low_points.append((x, y))
 
-print(sum(grid[x][y] + 1 for x, y in low_points))
+basin_sizes = []
+while low_points:
+    x, y = low_points.pop()
+    q = deque([(x, y)])
+    basin = set()
+
+    while q:
+        x, y = q.popleft()
+        if (x, y) in basin or grid[x][y] == 9:
+            continue
+
+        basin.add((x, y))
+        for dx, dy in dirs:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[nx]):
+                q.append((nx, ny))
+
+    basin_sizes.append(len(basin))
+
+print(prod(sorted(basin_sizes)[-3:]))
