754d56779016e8b18cb35e515798dc043e4bf947
Konstantine Machavariani <kotiko2004@gmail.com>
2025-07-31 10:35:19 +0400

    Solve AoC 2021 - Day 10 Part 2

--- Advent of Code/2021/10.py
+++ Advent of Code/2021/10.py
@@ -1,22 +1,30 @@
-cost = {')': 3, ']': 57, '}': 1197, '>': 25137}
-opposite = {'(': ')', '[': ']', '{': '}', '<': '>'}
+cost = {'(': 1, '[': 2, '{': 3, '<': 4}
+match = {'(': ')', '[': ']', '{': '}', '<': '>'}
 
 
 def parse(line):
     stack = []
     for c in line:
-        if c in '([{<':
+        if c in match:
             stack.append(c)
-        else:
-            if not stack:
-                return cost[c]
-
-            last = stack.pop()
-            if c != opposite[last]:
-                return cost[c]
-
-    return 0
+        elif not stack or match[stack.pop()] != c:
+            return None
+    return stack
 
 
-total = sum([parse(line) for line in open('input').read().splitlines()])
-print(total)
+def get_score(s):
+    score = 0
+    for c in reversed(s):
+        score = score * 5 + cost[c]
+    return score
+
+
+lines = open('input').read().splitlines()
+sums = [
+    get_score(stack)
+    for line in lines
+    if (stack := parse(line))
+]
+
+sums.sort()
+print(sums[len(sums) // 2])
