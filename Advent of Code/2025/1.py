ee92c7835bfb532a119274a15268127f84cfd177
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-01 14:00:16 +0400

    Solve AoC 2025 - Day 1 Part 2

--- Advent of Code/2025/1.py
+++ Advent of Code/2025/1.py
@@ -1,10 +1,34 @@
-dial = 50
-total = 0
-for line in open('input'):
-    line = line.strip()
-    direction = line[0]
-    num = int(line[1:])
-    dial = (dial + (num if direction == 'R' else -num)) % 100
-    if dial == 0: total += 1
+current_position = 50
+zero_count = 0
 
-print(total)
+for line in open('input').read().strip().split('\n'):
+    direction = line[0]
+    distance = int(line[1:])
+
+    if direction == 'R':
+        steps_to_zero = (100 - current_position) % 100
+        if steps_to_zero == 0 and current_position == 0:
+            steps_to_zero = 100
+        elif steps_to_zero == 0:
+            steps_to_zero = 100
+    else:
+        steps_to_zero = current_position
+
+        if steps_to_zero == 0 and current_position == 0:
+            steps_to_zero = 100
+        elif steps_to_zero == 0:
+            steps_to_zero = 100
+
+    if distance >= steps_to_zero:
+        remaining_distance = distance - steps_to_zero
+        additional_zeros = remaining_distance // 100
+
+        rotation_zeros = 1 + additional_zeros
+        zero_count += rotation_zeros
+
+    if direction == 'R':
+        current_position = (current_position + distance) % 100
+    else:
+        current_position = (current_position - distance) % 100
+
+print(zero_count)
