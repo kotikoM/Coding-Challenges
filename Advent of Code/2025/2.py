915ca04b3aa28a3ce3a12181553d62e21428657f
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-02 11:12:53 +0400

    Solve AoC 2025 - Day 2 Part 2

--- Advent of Code/2025/2.py
+++ Advent of Code/2025/2.py
@@ -1,26 +1,27 @@
 def repeated_numbers_in_range(L, R):
-    results = []
+    results = set()
 
     min_len = len(str(L))
     max_len = len(str(R))
 
     for total_len in range(min_len, max_len + 1):
-        if total_len % 2 == 1:
-            continue
+        for k in range(2, total_len + 1):
+            if total_len % k != 0:
+                continue
 
-        half_len = total_len // 2
-        start = 10 ** (half_len - 1)
-        end = 10 ** half_len - 1
+            block_len = total_len // k
 
-        for s in range(start, end + 1):
-            n = int(str(s) * 2)
-            if n > R:
-                break
-            if n >= L:
-                results.append(n)
+            start = 10 ** (block_len - 1)
+            end = 10 ** block_len - 1
+
+            for s in range(start, end + 1):
+                n = int(str(s) * k)
+                if n > R:
+                    break
+                if n >= L:
+                    results.add(n)
 
     return results
-
 
 print(sum(
     n
