e84941290dc102bbff71bcd27014e156f7c7b027
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-03 11:15:51 +0400

    Solve AoC 2025 - Day 3 Part 2

--- Advent of Code/2025/3.py
+++ Advent of Code/2025/3.py
@@ -1,10 +1,15 @@
 total = 0
 for l in open('input').read().splitlines():
-    numbers = list(map(int, l))
-
-    left = max(numbers[:-1])
-    i = numbers.index(left)
-    right = max(numbers[i + 1:])
-    total += left * 10 + right
+    nums = list(map(int, l))
+    k = 12
+    drop = len(nums) - k
+    out = []
+    for x in nums:
+        while drop and out and out[-1] < x:
+            out.pop()
+            drop -= 1
+        out.append(x)
+    out = out[:k]
+    total += int(''.join(map(str, out)))
 
 print(total)
