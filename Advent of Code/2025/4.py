23898a7d57f775995ca97cf9d4776ce6a24679bd
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-04 11:42:07 +0400

    Solve AoC 2025 - Day 4 Part 2

--- Advent of Code/2025/4.py
+++ Advent of Code/2025/4.py
@@ -1,18 +1,24 @@
 grid = [list(line) for line in open('input').read().split('\n')]
 
 accessible_rolls = 0
-for x in range(len(grid)):
-    for y in range(len(grid[0])):
-        if grid[x][y] != '@':
-            continue
+while True:
+    changed = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] != '@':
+                continue
 
-        neighbors = 0
-        for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-            nx, ny = x + dx, y + dy
-            if -1 < nx < len(grid) and -1 < ny < len(grid[0]) and grid[nx][ny] == '@':
-                neighbors += 1
+            neighbors = 0
+            for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                nx, ny = x + dx, y + dy
+                if -1 < nx < len(grid) and -1 < ny < len(grid[0]) and grid[nx][ny] == '@':
+                    neighbors += 1
 
-        if neighbors < 4:
-            accessible_rolls += 1
+            if neighbors < 4:
+                changed += 1
+                accessible_rolls += 1
+                grid[x][y] = '.'
+
+    if changed == 0: break
 
 print(accessible_rolls)
