365cdeab7cf86a985601ef092bffe17f95bd6ff7
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-11 17:28:27 +0400

    Solve EC 2025 - Quest 1 Part 2

--- Everybody Codes/2025/1.py
+++ Everybody Codes/2025/1.py
@@ -3,7 +3,7 @@ names, instructions = names.split(','), instructions.split(',')
 
 i = 0
 for d in instructions:
-    i += int(d[1]) * (1 if d[0] == 'R' else -1)
-    i = max(0, min(i, len(names) - 1))
+    i += int(d[1:]) * (1 if d[0] == 'R' else -1)
+    i = i % len(names)
 
 print(names[i])
