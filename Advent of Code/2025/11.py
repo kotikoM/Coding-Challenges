f4e9dbdde6743a0050c978b23bc196a46abea231
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-11 11:56:36 +0400

    Solve AoC 2025 - Day 11 Part 2

--- Advent of Code/2025/11.py
+++ Advent of Code/2025/11.py
@@ -1,12 +1,19 @@
+from functools import lru_cache
+
 devices = {
     i: o.split(' ')
     for l in open('input').read().split('\n')
     for i, o in [l.split(': ', 1)]
 }
 
+@lru_cache(None)
 def count_paths(start, end):
     if start == end:
         return 1
+    if start == 'out':
+        return 0
     return sum(count_paths(n, end) for n in devices[start])
 
-print(count_paths('you', 'out'))
+
+print(count_paths('svr', 'fft') * count_paths('fft', 'dac') * count_paths('dac', 'out') +
+      count_paths('svr', 'dac') * count_paths('dac', 'fft') * count_paths('fft', 'out'))
