c590e896a37dc6dca021b40164eecbd89db8b5e0
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-11 17:32:55 +0400

    Solve EC 2025 - Quest 1 Part 3

--- Everybody Codes/2025/1.py
+++ Everybody Codes/2025/1.py
@@ -1,9 +1,11 @@
 names, instructions = open('input').read().split('\n\n')
 names, instructions = names.split(','), instructions.split(',')
 
-i = 0
 for d in instructions:
-    i += int(d[1:]) * (1 if d[0] == 'R' else -1)
-    i = i % len(names)
+    direction = 1 if d[0] == 'R' else -1
+    steps = int(d[1:])
+    j = (direction * steps) % len(names)
 
-print(names[i])
+    names[0], names[j] = names[j], names[0]
+
+print(names[0])
